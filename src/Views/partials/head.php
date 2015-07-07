c4e86a2aa46aa774cb309e056a24dbc4b8dec915
John Jablonski <jablonski.kce@gmail.com>
2015-07-07 08:01:09 +0200

    Update application title in src\Views\partials\head.php

--- src/Views/partials/head.php
+++ src/Views/partials/head.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Laravel Console</title>
+        <title>PHP Eval Console</title>
         <meta name="viewport" content="width=device-width">
 
         <link rel="stylesheet" href="<?php echo $config['assets_url']; ?>/css/style.min.css" />
